7f5472247d7b707b3cb36a369f2cad71ccac33d5
suyash srivastava <suyash@primafelicitas.com>
2018-07-28 12:47:28 +0530

    next commit

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -4,28 +4,37 @@ const Web3 = require('web3');
 
 const web3 = new Web3(ganache.provider());
 
-class Car{
+// class Car{
 
-	park(){
-		return 'starts';
-	}
+// 	park(){
+// 		return 'starts';
+// 	}
 
-	drive(){
-		return 'virtual driver';
-	  }
-	}
-let car;
-beforeEach(()=>{
-	car =new Car;
-});
-	describe('Car',()=>{         
-		it('suyash',()=>{
-			
-			assert.equal(car.park(),'starts');
-		});
+// 	drive(){
+// 		return 'virtual driver';
+// 	  }
+// 	}
+// let car;
+let accounts;
+beforeEach(async ()=>{
+	accounts = web3.eth.getAccounts();
 
-		it('can drive',()=>{
-			
-			assert.equal(car.drive(),'virtual driver');
-		});
 	});
+});
+
+describe('Inbox',()=>{
+	it('deploys a contract',()=>{
+		console.log(accounts);
+	});
+});
+	// describe('Car',()=>{         
+	// 	it('suyash',()=>{
+			
+	// 		assert.equal(car.park(),'starts');
+	// 	});
+
+	// 	it('can drive',()=>{
+			
+	// 		assert.equal(car.drive(),'virtual driver');
+	// 	});
+	// });
